e9b73b739ba432d74214be097da2b94b72eac511
orion-7 <serd.ig@yandex.ru>
2026-02-18 19:45:22 +0300

    Убираем лишнее

--- config.py
+++ config.py
@@ -10,22 +10,22 @@ MAX_ACTIVE_MESSAGES = 40
 DATABASE_URL = os.getenv('DATABASE_URL')
 TEST_DB_URL = os.getenv('TEST_DB_URL')
 
-DATA_DIR = os.getenv('DATA_DIR', 'data')
-DB_PATH = os.path.join(DATA_DIR, 'history.db')
-if not os.path.exists(DATA_DIR):
-    try:
-        os.makedirs(DATA_DIR)
-        print(f"✅ Папка {DATA_DIR} успешно создана")
-    except PermissionError:
-        # Если нет прав - возможно, это примонтированный диск
-        # Проверяем, доступна ли папка для записи
-        print(f"⚠️ Нет прав на создание {DATA_DIR}, но папка может быть примонтирована")
-        if not os.path.exists(DATA_DIR):
-            # Если папки всё равно нет - откатываемся на локальную
-            print(f"❌ Папка {DATA_DIR} недоступна, используем './data'")
-            DATA_DIR = 'data'
-            DB_PATH = os.path.join(DATA_DIR, 'history.db')
-            os.makedirs(DATA_DIR, exist_ok=True)
+# DATA_DIR = os.getenv('DATA_DIR', 'data')
+# DB_PATH = os.path.join(DATA_DIR, 'history.db')
+# if not os.path.exists(DATA_DIR):
+#     try:
+#         os.makedirs(DATA_DIR)
+#         print(f"✅ Папка {DATA_DIR} успешно создана")
+#     except PermissionError:
+#         # Если нет прав - возможно, это примонтированный диск
+#         # Проверяем, доступна ли папка для записи
+#         print(f"⚠️ Нет прав на создание {DATA_DIR}, но папка может быть примонтирована")
+#         if not os.path.exists(DATA_DIR):
+#             # Если папки всё равно нет - откатываемся на локальную
+#             print(f"❌ Папка {DATA_DIR} недоступна, используем './data'")
+#             DATA_DIR = 'data'
+#             DB_PATH = os.path.join(DATA_DIR, 'history.db')
+#             os.makedirs(DATA_DIR, exist_ok=True)
 
 if __name__ == "__main__":
     print(f"Token: {'Found' if TOKEN else 'Not found'}")
