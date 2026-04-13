e752390684d726416511e2e6d6221658cb15a9bf
orion-7 <serd.ig@yandex.ru>
2026-04-13 21:18:02 +0300

    Мелкие изменения

--- config.py
+++ config.py
@@ -4,15 +4,13 @@ from dotenv import load_dotenv  #Импортируем из библиотек�
 load_dotenv()  # Функция автоматически находит и читает .env
 FOLDER_KEY = os.getenv('FOLDER_ID')
 API_KEY = os.getenv('OPENROUTER_API_KEY')
-LLM_API_URL = 'https://openai.bothub.ru/v1/chat/completions'
+LLM_API_URL = "https://openai.bothub.chat/v1"
 # 'https://openrouter.ai/api/v1/chat/completions'
 
 TOKEN = os.getenv('BOT_TOKEN') # Найдет значение по имени и запомнит в переменную
 MAX_ACTIVE_MESSAGES = 100
 DATABASE_URL = os.getenv('DATABASE_URL')
 TEST_DB_URL = os.getenv('TEST_DB_URL')
-
-DEFAULT_MODEL = "google/gemma-4-31b-it:free"  # Основная модель
 
 if __name__ == "__main__":
     print(f"Token: {'Found' if TOKEN else 'Not found'}")
