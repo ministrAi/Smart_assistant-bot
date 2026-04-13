a61e6c89cd0d05b3321114ef27963a3553565537
orion-7 <serd.ig@yandex.ru>
2026-04-13 23:20:39 +0300

    Мелкие изменения

--- services/ai_manager.py
+++ services/ai_manager.py
@@ -32,7 +32,7 @@ async def get_ai_response(history):
     full_messages = [system_prompt] + trimmed_history
     # Подготовка тела запроса
     payload = {
-        "model": "minimax-m2.7",
+        "model": "deepseek-v3.2",
         "messages": full_messages,
         "temperature": 0.5,
         "max_tokens": 700,
