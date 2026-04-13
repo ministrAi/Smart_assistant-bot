3d8611c92a7d8c5d2c2dbf2f33392336ee9d6719
orion-7 <serd.ig@yandex.ru>
2026-04-13 21:36:32 +0300

    Мелкие изменения

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv  #Импортируем из библиотек�
 load_dotenv()  # Функция автоматически находит и читает .env
 FOLDER_KEY = os.getenv('FOLDER_ID')
 API_KEY = os.getenv('OPENROUTER_API_KEY')
-LLM_API_URL = "https://openai.bothub.ru/v1"
-# 'https://openrouter.ai/api/v1/chat/completions'
+LLM_API_URL = "https://openai.bothub.chat/v1/chat/completions"
 
 TOKEN = os.getenv('BOT_TOKEN') # Найдет значение по имени и запомнит в переменную
 MAX_ACTIVE_MESSAGES = 100
