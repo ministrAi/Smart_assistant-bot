bb21c137992a3cdb725414a8a9d40d46f89c14fc
orion-7 <serd.ig@yandex.ru>
2026-06-26 06:34:31 +0300

    - Сменил модель

--- services/ai_manager.py
+++ services/ai_manager.py
@@ -22,9 +22,9 @@ async def call_llm(messages: list) -> str:
     payload = {
 
         # "model": "glm-5.2",
-        # "model": "deepseek-v4-flash",
+        "model": "deepseek-v4-flash",
         # "model": "gpt-5.4-mini",
-        "model": "claude-haiku-4.5",
+        # "model": "claude-haiku-4.5",
         # "model": "deepseek-chat-v3.1",
         "messages": messages,
         "temperature": 0.5,
